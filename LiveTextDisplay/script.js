78527085f8560a06a4908aaca3a613fac7bb6cc8
SanjuVijayakumar <33987375+SanjuVijayakumar@users.noreply.github.com>
2026-05-13 09:23:44 +0530

    Delete LiveTextDisplay directory

--- LiveTextDisplay/script.js
+++
@@ -1,8 +0,0 @@
-function updateDisplay() {
-    const textInput = document.getElementById('textInput');
-    const displayText = document.getElementById('displayText');
-    displayText.textContent = textInput.value;
-}
-
-const textInput = document.getElementById('textInput');
-textInput.addEventListener('input', updateDisplay);
